4f02d5c0c539ae1b0e3fe678237402aa1de6a5e5
harrisonpim <harrison.stroma@gmail.com>
2019-12-09 13:29:43 +0000

    add feature-similarity api

---
+++ terraform/apis/main.tf
@@ -0,0 +1,81 @@
+module "palette_api" {
+  source = "./modules/service/api"
+
+  namespace = "palette-api"
+
+  container_image = "wellcome/palette_api:latest"
+  container_port  = "9001"
+
+  namespace_id = "${aws_service_discovery_private_dns_namespace.namespace.id}"
+
+  cluster_id = "${aws_ecs_cluster.cluster.name}"
+
+  vpc_id = "${local.vpc_id}"
+
+  security_group_ids = [
+    "${aws_security_group.service_egress_security_group.id}",
+    "${aws_security_group.service_lb_ingress_security_group.id}",
+  ]
+
+  assumable_roles = ["${local.miro_read_role}"]
+
+  subnets                      = ["${local.private_subnets}"]
+  nginx_container_port         = "${local.nginx_listener_port}"
+  nginx_container_image        = "${local.nginx_container_image}"
+  env_vars                     = {}
+  env_vars_length              = 0
+  lb_arn                       = "${module.nlb.arn}"
+  lb_dns_name                  = "${module.nlb.dns_name}"
+  api_gateway_rest_api_id      = "${aws_api_gateway_rest_api.apis.id}"
+  api_gateway_root_resource_id = "${aws_api_gateway_rest_api.apis.root_resource_id}"
+  api_gateway_vpc_link_id      = "${aws_api_gateway_vpc_link.apis.id}"
+  listener_port                = "7000"
+  cpu                          = 2048
+  memory                       = 4096
+  sidecar_cpu                  = 1024
+  sidecar_memory               = 2048
+  app_cpu                      = 1024
+  app_memory                   = 2048
+  task_desired_count           = 1
+}
+
+module "feature_similarity" {
+  source = "./modules/service/api"
+
+  namespace = "feature-similarity"
+
+  container_image = "wellcome/feature_similarity:latest"
+  container_port  = "9001"
+
+  namespace_id = "${aws_service_discovery_private_dns_namespace.namespace.id}"
+
+  cluster_id = "${aws_ecs_cluster.cluster.name}"
+
+  vpc_id = "${local.vpc_id}"
+
+  security_group_ids = [
+    "${aws_security_group.service_egress_security_group.id}",
+    "${aws_security_group.service_lb_ingress_security_group.id}",
+  ]
+
+  assumable_roles = ["${local.miro_read_role}"]
+
+  subnets                      = ["${local.private_subnets}"]
+  nginx_container_port         = "${local.nginx_listener_port}"
+  nginx_container_image        = "${local.nginx_container_image}"
+  env_vars                     = {}
+  env_vars_length              = 0
+  lb_arn                       = "${module.nlb.arn}"
+  lb_dns_name                  = "${module.nlb.dns_name}"
+  api_gateway_rest_api_id      = "${aws_api_gateway_rest_api.apis.id}"
+  api_gateway_root_resource_id = "${aws_api_gateway_rest_api.apis.root_resource_id}"
+  api_gateway_vpc_link_id      = "${aws_api_gateway_vpc_link.apis.id}"
+  listener_port                = "7001"
+  cpu                          = 2048
+  memory                       = 4096
+  sidecar_cpu                  = 1024
+  sidecar_memory               = 2048
+  app_cpu                      = 1024
+  app_memory                   = 2048
+  task_desired_count           = 1
+}
