1b974efbc768137b30ff1034befdb78807d1d967
harrisonpim <harrison.stroma@gmail.com>
2020-03-10 12:52:57 +0000

    describe imagenet magic in variable names

--- apis/feature_extraction/inference/app/feature_extraction.py
+++ apis/feature_extraction/inference/app/feature_extraction.py
@@ -8,13 +8,16 @@ device = (
     else torch.device('cpu')
 )
 
+imagenet_mean = [0.485, 0.456, 0.406]
+imagenet_std = [0.229, 0.224, 0.225]
+
 transform_pipeline = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.Normalize(mean=imagenet_mean, std=imagenet_std)
 ])
 
-feature_extractor = vgg16(pretrained=True).to(device).eval()
+feature_extractor = vgg16(pretrained=True, progress=False).to(device).eval()
 feature_extractor.classifier = feature_extractor.classifier[:4]
 
 
