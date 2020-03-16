263823ec74bcca5eb772bcda1bd5ea787e91abe5
Harrison Pim <harrison.stroma@gmail.com>
2020-03-16 17:03:36 +0000

    logstash port as an environment variable
    Co-Authored-By: Jamie Parkinson <j.parkinson@wellcome.ac.uk>

--- apis/feature_extraction/inference/app/src/logging.py
+++ apis/feature_extraction/inference/app/src/logging.py
@@ -7,7 +7,7 @@ def get_logstash_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     logstash_handler = logstash.LogstashHandler(
-        os.environ['LOGSTASH_HOST'], 5000,
+		os.environ['LOGSTASH_HOST'], os.environ['LOGSTASH_PORT'],
         version=1
     )
     logger.addHandler(logstash_handler)
