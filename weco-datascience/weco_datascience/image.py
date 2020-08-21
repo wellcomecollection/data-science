aabe000cd3c3e5fc3ea5439540fa031070c3ef60
harrisonpim <harrison.stroma@gmail.com>
2020-08-21 17:58:55 +0100

    add size to get_image_from_url()

--- weco-datascience/weco_datascience/image.py
+++ weco-datascience/weco_datascience/image.py
@@ -8,14 +8,17 @@ from PIL import Image
 from .http import fetch_url_bytes
 
 
-async def get_image_from_url(image_url):
+async def get_image_from_url(image_url, size=None):
     image_url = unquote_plus(image_url)
     parsed_url = urlparse(image_url)
     if parsed_url.scheme == "file":
         image_pointer = await get_local_image(parsed_url.path)
     else:
         image_pointer = await get_remote_image(image_url)
-    return Image.open(BytesIO(image_pointer))
+    image = Image.open(BytesIO(image_pointer))
+    if size:
+        image = image.resize((size, size), resample=Image.BILINEAR)
+    return image
 
 
 async def get_local_image(path):
@@ -42,15 +45,15 @@ async def get_remote_image(url):
         raise ValueError(f"{url} is not a valid image URL")
 
 
-def get_image_url_from_iiif_url(iiif_url, size=(224, 224)):
+def get_image_url_from_iiif_url(iiif_url, input_size=224):
     try:
         image = IIIFImageClient.init_from_url(iiif_url)
     except ParseError:
         raise ValueError(f"{iiif_url} is not a valid iiif URL")
 
     if "dlcs" in image.api_endpoint:
-        # DLCS provides a thumbnails service which only serves certain sizes
-        # of image. Requests for these don't touch the image server and so, as
+        # DLCS provides a thumbnails service which only serves certain sizes of
+        # image. Requests for these don't touch the image server and so, as
         # we're performing lots of requests, we use 400x400 thumbnails and
         # resize them ourselves later on.
         image.api_endpoint = image.api_endpoint.replace("iiif-img", "thumbs")
@@ -58,4 +61,4 @@ def get_image_url_from_iiif_url(iiif_url, size=(224, 224)):
         # https://iiif.io/api/image/1.0/#4-2-size
         return str(image.size(width=400, height=400, exact=True))
 
-    return str(image.size(width=size[0], height=size[1], exact=False))
+    return str(image.size(width=input_size, height=input_size, exact=False))
