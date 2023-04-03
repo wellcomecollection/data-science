37ee12a4b4ea494ce2823994b660deb7c4ac5871
Alex Chan <alex@alexwlchan.net>
2023-04-03 13:47:46 +0100

    keep fiddling!

--- terraform/modules/python_api/load_balancer.tf
+++ terraform/modules/python_api/load_balancer.tf
@@ -1,6 +1,36 @@
-resource "aws_security_group" "allow_all_ingress" {
-  name        = "${var.name}-all_ingress"
-  description = "Allow traffic from anywhere on the Internet"
+data "aws_vpc" "vpc" {
+  id = var.vpc_id
+}
+
+resource "aws_security_group" "load_balancer" {
+  name        = "${var.name}-load_balancer"
+  description = "Allow traffic between services and the load balancer"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [data.aws_vpc.vpc.cidr_block]
+  }
+}
+
+resource "aws_security_group" "interservice" {
+  name        = "${var.name}-interservice"
   vpc_id      = var.vpc_id
 
   ingress {
@@ -8,6 +38,19 @@ resource "aws_security_group" "allow_all_ingress" {
     to_port   = 0
     protocol  = "-1"
     self      = true
+  }
+}
+
+resource "aws_security_group" "allow_all_ingress" {
+  name        = "${var.name}-all_ingress"
+  description = "Allow traffic from anywhere on the Internet"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -27,7 +70,7 @@ resource "aws_security_group" "allow_all_egress" {
 resource "aws_alb" "alb" {
   name            = var.name
   subnets         = var.subnets
-  security_groups = [aws_security_group.allow_all_ingress.id, aws_security_group.allow_all_egress.id]
+  security_groups = [aws_security_group.load_balancer.id, aws_security_group.interservice.id, aws_security_group.allow_all_ingress.id, aws_security_group.allow_all_egress.id]
 }
 
 resource "aws_alb_target_group" "ecs_service_default" {
