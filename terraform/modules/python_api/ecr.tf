2c0eb353e1ab5f06fa8980eba869a61ffb1aee38
Alex Chan <alex@alexwlchan.net>
2023-04-06 13:21:15 +0100

    Allow destroying the ECR repo with `terraform destroy`

--- terraform/modules/python_api/ecr.tf
+++ terraform/modules/python_api/ecr.tf
@@ -1,3 +1,10 @@
 resource "aws_ecr_repository" "api" {
   name = "weco/${var.name}"
+
+  # This will delete the ECR repository when you run `terraform destroy`.
+  #
+  # In general we don't want this because it's potentially disruptive,
+  # but since this is only used for short-lived experiments and
+  # ephemeral apps, it's fine.
+  force_delete = true
 }
