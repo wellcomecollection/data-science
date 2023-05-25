3b8e865a5f4d70474185754b80c1b3f208e6f4fc
harrisonpim <hello@harrisonpim.com>
2023-05-25 20:14:41 +0100

    use a single colour embedding

--- knn-colours/webapp/components/result.tsx
+++ knn-colours/webapp/components/result.tsx
@@ -10,33 +10,6 @@ export const Result = ({ result }: { result: ResultType }) => {
         width={1000}
         height={1000}
       />
-      <div className="flex h-2 flex-row flex-wrap">
-        <div
-          key={result.colors.a.hex}
-          className="w-1/5"
-          style={{ backgroundColor: result.colors.a.hex }}
-        />
-        <div
-          key={result.colors.b.hex}
-          className="w-1/5"
-          style={{ backgroundColor: result.colors.b.hex }}
-        />
-        <div
-          key={result.colors.c.hex}
-          className="w-1/5"
-          style={{ backgroundColor: result.colors.c.hex }}
-        />
-        <div
-          key={result.colors.d.hex}
-          className="w-1/5"
-          style={{ backgroundColor: result.colors.d.hex }}
-        />
-        <div
-          key={result.colors.e.hex}
-          className="w-1/5"
-          style={{ backgroundColor: result.colors.e.hex }}
-        />
-      </div>
     </div>
   );
 };
