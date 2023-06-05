20f2229e645108ab2f03ac4f247bed62c8076d79
harrisonpim <hello@harrisonpim.com>
2023-06-05 10:51:41 +0100

    beep

--- knn-colours/pipeline/main.py
+++ knn-colours/pipeline/main.py
@@ -22,7 +22,7 @@ with open("data/index_config/images.json", "r", encoding="utf-8") as f:
 
 index_config = json.loads(
     json.dumps(index_config).replace(
-        "{{EMBEDDING_DIMENSIONALITY}}", str(n_bins ** 3)
+        "{{EMBEDDING_DIMENSIONALITY}}", str(n_bins**3)
     )
 )
 
@@ -36,40 +36,39 @@ es.indices.create(index=index_name, **index_config)
 
 color_embedder = ColorEmbedder(n_bins=n_bins)
 
-
-
 progress_bar = tqdm(
-    yield_source_images(pipeline_date="2023-03-29"), 
-    total=count_source_images(pipeline_date="2023-03-29")
+    yield_source_images(pipeline_date="2023-03-29"),
+    total=count_source_images(pipeline_date="2023-03-29"),
 )
 
 for image_data in progress_bar:
-    log.debug(
-        f"Processing image {image_data['id']}"
-        f"{progress_bar.n} / {progress_bar.total}"
-    )
-    progress_bar.set_description(f'Processing {image_data["id"]}')
+    try:
+        log.debug(
+            f"Processing image {image_data['id']} "
+            f"{progress_bar.n}/{progress_bar.total}"
+        )
+        progress_bar.set_description(f'Processing {image_data["id"]}')
 
-    # get the image
-    iiif_url = image_data["thumbnail"]["url"]
-    thumbnail_url = iiif_url.replace(
-        "info.json",
-        "full/!400,400/0/default.jpg",
-    )
-    thumbnail = httpx.get(thumbnail_url).content
-    image = Image.open(BytesIO(thumbnail))
-    embedding = color_embedder.embed(image)
+        # get the image
+        iiif_url = image_data["thumbnail"]["url"]
+        thumbnail_url = iiif_url.replace(
+            "info.json",
+            "full/!400,400/0/default.jpg",
+        )
+        thumbnail = httpx.get(thumbnail_url, timeout=30).content
+        image = Image.open(BytesIO(thumbnail))
+        embedding = color_embedder.embed(image)
 
-    document = {
-        "thumbnail_url": thumbnail_url,
-        "image_id": image_data["id"],
-        "source_id": image_data["source"]["id"],
-        "title": image_data["source"]["title"],
-        "embedding": embedding.tolist(),
-    }
+        document = {
+            "thumbnail_url": thumbnail_url,
+            "image_id": image_data["id"],
+            "source_id": image_data["source"]["id"],
+            "title": image_data["source"]["title"],
+            "embedding": embedding.tolist(),
+        }
 
-    es.index(
-        index=index_name,
-        document=document,
-        id=image_data["id"],
-    )
+        es.index(index=index_name, document=document, id=image_data["id"])
+    except Exception as e:
+        log.error(f"Error processing image {image_data['id']}: {e}")
+
+    progress_bar.update(1)
--- knn-colours/pipeline/src/source.py
+++ knn-colours/pipeline/src/source.py
@@ -1,12 +1,31 @@
-from elasticsearch.helpers import scan
 from .elasticsearch import get_catalogue_elastic_client
+
 
 def yield_source_images(pipeline_date):
     es = get_catalogue_elastic_client(pipeline_date)
     index_name = f"images-indexed-{pipeline_date}"
 
-    for image_data in scan(es, index=index_name):
-        yield image_data["_source"]["display"]
+    pit = es.open_point_in_time(index=index_name, keep_alive="12h")
+    search_after = None
+    while True:
+        results = es.search(
+            body={
+                "size": 100,
+                "query": {"match_all": {}},
+                "_source": ["query.id", "display"],
+                "sort": [{"query.id": "asc"}],
+                "pit": {"id": pit["id"], "keep_alive": "1m"},
+                "search_after": search_after
+            },
+        )
+
+        for hit in results["hits"]["hits"]:
+            yield hit["_source"]["display"]
+        if len(results["hits"]["hits"]) < 100:
+            break
+
+        search_after = [results["hits"]["hits"][-1]["_source"]["query"]["id"]]
+    es.close_point_in_time(id=pit["id"])
 
 
 def count_source_images(pipeline_date):
